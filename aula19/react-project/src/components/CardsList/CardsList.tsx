b3c4386cf678a8be2a09112c957cb80e1496682b
ArthurRocha2100 <arthurrocha2101@gmail.com>
2024-01-13 02:37:11 -0300

    Começando o primeiro projeto React. Implementando a tela da aula 6 com o React

---
+++ aula19/react-project/src/components/CardsList/CardsList.tsx
@@ -0,0 +1,51 @@
+import { Card } from "../Card/Card";
+import styles from './CardsList.module.css'
+
+const CardArray = [
+    {
+        imageUrl: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png',
+        name: 'Fulano',
+        office: 'Dev'
+    },
+    {
+        imageUrl: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png',
+        name: 'Fulano',
+        office: 'Dev'
+    },  
+    {
+        imageUrl: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png',
+        name: 'Fulano',
+        office: 'Dev'
+    },    
+    {
+        imageUrl: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png',
+        name: 'Fulano',
+        office: 'Dev'
+    },
+    {
+        imageUrl: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png',
+        name: 'Fulano',
+        office: 'Dev'
+    },
+    {
+        imageUrl: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png',
+        name: 'Fulano',
+        office: 'Dev'
+    },
+]
+
+export const CardsList = () => {
+    return(
+        <div className={styles.cardsList}>
+            {
+                CardArray.map(card => (
+                    <Card 
+                        imageUrl={card.imageUrl}
+                        name={card.name}
+                        office={card.office}
+                    />
+                ))
+            }
+        </div>
+    )
+}
